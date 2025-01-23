7683e61f0b49ec2e0aa4b491f7c0d806ae5daf43
Abhijeet <abhijeet.gotkhindi@gmail.com>
2025-01-23 23:17:30 +0530

    master

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 
 // Routes
-// router.post('/', loginController.validatedUser);
+// router.post('/', loginController.validatedUser); //old
 
 router.post('/', function(req, res, next) {
     loginController.validatedUser(req, res);
